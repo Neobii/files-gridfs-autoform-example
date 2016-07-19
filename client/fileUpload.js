de2587350813b67fe0eed8ed9bc1e38358904ad4
neobii <iwilldevelopcodeforyou@gmail.com>
2016-07-19 12:50:16 -0500

    #2 ability to specify collection name

--- client/fileUpload.js
+++ client/fileUpload.js
@@ -1,5 +1,6 @@
 Template.afFileUpload.onCreated(function () {
-  this.imageId = new ReactiveVar(false);
+  this.collectionName = () => this.data.atts.collection
+  this.fileId = new ReactiveVar(false);
   this.currentUpload = new ReactiveVar(false);
 });
 
@@ -9,25 +10,25 @@ Template.afFileUpload.helpers({
   currentUpload: function () {
     return Template.instance().currentUpload.get();
   },
-  imageId() {
-    return Template.instance().imageId.get();
+  fileId() {
+    return Template.instance().fileId.get();
   },
-  uploadedImage() {
-    let imgId = Template.instance().imageId.get();
-    let image = Images.find({_id: imgId});
-    return image.cursor;
+  uploadedFile() {
+    let fId = Template.instance().fileId.get();
+    let file = global[Template.instance().collectionName()].find({_id: fId});
+    return file.cursor;
   }
 });
 
 Template.afFileUpload.events({
-  'click [data-action="remove-image"]'(e, t) {
-    t.imageId.set(false);
+  'click [data-action="remove-file"]'(e, t) {
+    t.fileId.set(false);
   },
   'change .af-file-upload-capture': function (e, template) {
     if (e.currentTarget.files && e.currentTarget.files[0]) {
       // We upload only one file, in case 
       // multiple files were selected
-      var upload = Images.insert({
+      var upload = global[template.collectionName()].insert({
         file: e.currentTarget.files[0],
         streams: 'dynamic',
         chunkSize: 'dynamic'
@@ -42,7 +43,7 @@ Template.afFileUpload.events({
           //alert('Error during upload: ' + error);
         } else {
           //alert('File "' + fileObj.name + '" successfully uploaded');
-          template.imageId.set(fileObj._id);
+          template.fileId.set(fileObj._id);
         }
         template.currentUpload.set(false);
       });
