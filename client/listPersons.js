4f62187bf6ab05ef5e4139dc56e6609ac6c76800
Dmitriy A. Golev <ceo@veliov.com>
2016-08-21 06:04:08 +0300

    Updates
     - Update Meteor to 1.4.1
     - Update MF to 1.7.0
     - Update AFF to 1.0.4
     - Minor fixes and enhancements

--- client/listPersons.js
+++ client/listPersons.js
@@ -1,14 +1,26 @@
 Template.listPersons.onCreated(function(){
-	this.autorun(() => {
-		this.subscribe("persons");
-	})
-})
+  this.update = new ReactiveVar(false);
+  this.autorun(() => {
+    this.subscribe("persons");
+  });
+});
 
 Template.listPersons.helpers({
-	persons() {
-		return Persons.find({});
-	},
-	getFormId() {
-		return "updatePerson" + this._id;
-	}
-})
+  persons() {
+    return Persons.find({});
+  },
+  getFormId() {
+    return "updatePerson" + this._id;
+  },
+  update(_id) {
+    return Template.instance().update.get() === _id
+  }
+});
+
+Template.listPersons.events({
+  'click [data-button-update]'(e, template) {
+    e.preventDefault();
+    template.update.set(template.update.get() === this._id ? false : this._id);
+    return false;
+  }
+});
